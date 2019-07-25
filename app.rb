d9826005a45bab40c5b0b5e6cf45f9f3ca5b741c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-25 02:43:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    puts @analyzed_text.most_used_letter.keys.first
     erb :results
   end
 end
